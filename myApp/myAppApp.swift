5a4f944603676e95d41df71ec91564ee99d070ad
cpa_17 <pascal.ahlner@gmx.de>
2022-06-14 15:55:10 +0200

    Initial Commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Pascal Ahlner on 14.06.22.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
